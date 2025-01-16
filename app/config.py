1e52384dcd0bd13a5658af0b2dbd351095efb6a9
Chris Ginn <chris_ginn@swissre.com>
2025-01-16 08:07:44 -0600

    fix: update the database connection (host.docker.internal)  and get query working.

--- app/config.py
+++ app/config.py
@@ -10,8 +10,8 @@ class Config:
     """
     POSTGRES_USER = os.getenv('POSTGRES_USER', 'default_user')
     POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'default_password')
-    POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')
-    POSTGRES_DB = os.getenv('POSTGRES_DB', 'default_db')
+    POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'host.docker.internal')
+    POSTGRES_DB = os.getenv('POSTGRES_DB', 'quakedb')
 
     # Construct the SQLAlchemy database URI
     SQLALCHEMY_DATABASE_URI = (
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import Config
 
@@ -16,15 +17,31 @@ def get_earthquake_by_id(id):
     Fetch a single earthquake record by ID.
     """
     try:
-        query = text("SELECT * FROM quakedb WHERE time=:id")
-        result = session.execute(query, {'id': id}).fetchone()
-        if result:
-            return jsonify(dict(result)), 200   
+        # query = text("SELECT * FROM earthquake_data WHERE id=:id")
+        # result = session.execute(query, {'id': id}).fetchone()
+        quake = session.query(EarthquakeData).filter(EarthquakeData.id == id).first()
+        print(quake)
+        if quake:
+            return jsonify(row2dict(quake)), 200
         else:
             return jsonify({"error": "Record not found"}), 404
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
+def row2dict(row):
+    d = {}
+    for column in row.__table__.columns:
+        d[column.name] = str(getattr(row, column.name))
+
+    return d
+
+
+class EarthquakeData(declarative_base()):
+    __tablename__ = 'earthquake_data'
+    id = Column(Integer, primary_key=True)
+    place = Column(String)
+    status = Column(String)
 
 if __name__ == '__main__':
     app.run(debug=True)
